6bba025dc83822e948b01c8254d5bbedca6cac22
sh4d0w_mac <467194403@qq.com>
2025-12-01 17:10:04 +0800

    s1

---
+++ frontend/src/views/guest/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>myBlog 个人博客系统</h1>
+    <p>前端项目初始化成功！</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+h1 {
+  color: #409eff;
+}
+</style>
